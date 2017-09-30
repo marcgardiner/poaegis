4d6e8bb7d85b280c43d0bc0896a302d2113b80ac
devm <servicemanager3386@yandex.com>
2017-09-30 10:28:35 -0400

    building structure

---
+++ config/config.py
@@ -0,0 +1,43 @@
+# config/config.py
+
+
+import os
+
+
+class Config(object):
+    """Environment Configuration Class"""
+
+    DEBUG = True
+    TESTINING = False
+    CSRF_TOKEN = True
+    SECRET = os.getenv('SECRET_KEY')
+
+    DB_URL = os.getenv('db_url')
+    DB_USER = os.getenv('db_user')
+    DB_PASSWORD = os.getenv('db_password')
+    DB_PORT = os.getenv('db_port')
+
+
+class DevConfig(Config):
+    """Dev Environment"""
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    """Testing Environment"""
+    DEBUG = True
+    TESTING = True
+
+
+class StatingConfig(Config):
+    """Staging Environment"""
+    DEBUG = True
+
+
+class ProductionConfiig(Config):
+    """
+        Production Environment, Hosted in AWS
+    """
+    DEBUG = False
+    TESTINING = False
+    
