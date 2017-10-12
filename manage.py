cd0ffeecc908bf557bd30d778ec9bbeb4fa60356
devm <servicemanager3386@yandex.com>
2017-10-12 11:37:53 -0400

    add command 'test'

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 #manage.py
 
 import os
+import unittest
 
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
@@ -17,6 +18,15 @@ manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
 
+@manager.command
+def test():
+    """test command"""
+    tests = unittest.TestLoader().discover('./tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasScuccessful():
+        return 0
+    return 1
+
+
 if __name__ == '__main__':
     manager.run()
-    
